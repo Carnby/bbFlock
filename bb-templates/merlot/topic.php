f5d811a5fda3c99e02c8caa0356f644dd5f1503d
Eduardo Graells <eduardo.graells@gmail.com>
2012-04-13 00:09:50 +0200

    cleaner topic template

--- bb-templates/merlot/topic.php
+++ bb-templates/merlot/topic.php
@@ -1,6 +1,5 @@
 <?php bb_get_header(); ?>
 
-
 <?php if ($posts) : ?>
 
 <?php gs_topic_pagination(); ?>
@@ -11,12 +10,11 @@
 
     <?php 
     $i = 1;
-    foreach ($posts as $bb_post) : ?>
+    foreach ($posts as $bb_post) { ?>
 	    <div id="post-<?php post_id(); ?>" <?php alt_class('post', post_del_class() . ' post number-' . $i++); ?>>
 	    <?php bb_post_template(); ?>
 	    </div>
-    <?php endforeach; ?>
-
+    <?php } ?>
 
     <div class="clearit"><br style=" clear: both;" />
 </div>
@@ -25,13 +23,15 @@
 
 <?php endif; ?>
 
-<?php if ( topic_is_open( $bb_post->topic_id ) ) : ?>
-    <?php post_form(); ?>
-    <?php else : ?>
-    <?php gs_post_form_open(); ?>
+<?php 
+if (topic_is_open(get_topic_id())) {
+    post_form();
+} else {
+    gs_post_form_open();
+    ?>
     <h2><?php _e('Topic Closed') ?></h2>
     <p><?php _e('This topic has been closed to new replies.') ?></p>
-    <?php gs_post_form_close(); ?>
-    <?php endif; ?>
+    <?php gs_post_form_close(); 
+} ?> 
 
 <?php bb_get_footer(); ?>
