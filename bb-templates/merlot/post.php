838afae349ec0286cdf3aa7c3902a765cb5bde9e
Eduardo Graells <eduardo.graells@gmail.com>
2012-04-28 16:47:23 +0200

    small change in layout for topic posts

--- bb-templates/merlot/post.php
+++ bb-templates/merlot/post.php
@@ -1,12 +1,16 @@
-<div class="post_author span2">
-	<?php post_author_avatar(); ?>
+<div class="post_author span1">
 	<p>
-		<strong><a href="<?php user_profile_link(get_post_author_id()); ?>"><?php post_author(); ?></a></strong><br />
-		<span class="label label-info"><?php echo  get_post_author_title(); ?></span>
+		<?php post_author_avatar(); ?><br />
+		
 	</p>
 </div>
 
-<div class="post_content span10">
+<div class="post_content span11">
+    <div class="post_stuff pull-left">
+        <strong><a href="<?php user_profile_link(get_post_author_id()); ?>"><?php post_author(); ?></a></strong>
+        <span class="label label-info"><?php echo get_post_author_title(); ?></span>
+    </div>
+    
     <div class="post_stuff pull-right">
         <?php gs_post_info(); ?>
     </div>
--- bb-templates/merlot/topic.php
+++ bb-templates/merlot/topic.php
@@ -9,14 +9,14 @@
 <div id="thread" class="row start-<?php echo $list_start; ?>">
 
     <?php 
-    $i = 1;
+    
     foreach ($posts as $bb_post) { ?>
-	    <div id="post-<?php post_id(); ?>" <?php alt_class('post', post_del_class() . ' post number-' . $i++); ?>>
-	    <?php bb_post_template(); ?>
+	    <div id="post-<?php post_id(); ?>" <?php alt_class('post', post_del_class() . ' post'); ?>>
+	        <?php bb_post_template(); ?>
 	    </div>
     <?php } ?>
 
-    <div class="clearit"><br style=" clear: both;" />
+    <div class="clearfix"></div>
 </div>
 
 <?php gs_topic_pagination(); ?>
@@ -27,8 +27,7 @@
 if (topic_is_open(get_topic_id())) {
     post_form();
 } else {
-    gs_post_form_open();
-    ?>
+    gs_post_form_open(); ?>
     <h2><?php _e('Topic Closed') ?></h2>
     <p><?php _e('This topic has been closed to new replies.') ?></p>
     <?php gs_post_form_close(); 
