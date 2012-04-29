9d6b42dfa51ac9c3533ba9d9a7108b6decfb609a
Eduardo Graells <eduardo.graells@gmail.com>
2012-04-29 03:28:34 +0200

    first version of the forum permissions plugin, which allows to setup which roles will be able to see each forum

---
+++ bb-plugins/bb-forum-permissions/bb-forum-permissions.php
@@ -0,0 +1,129 @@
+<?php
+/*
+Plugin Name: Forum Permissions
+Description: Setups minimum capabilities to browse through forums.
+Author: Eduardo Graells
+Author URI: http://about.me/egraells
+Version: 1.0
+License: GPLv3
+*/
+
+function bb_forum_permissions($forum_id = 0) {
+    global $bb_current_user;
+    global $bb_roles;
+    
+    if (!$forum_id)
+        return;
+    
+    $allowed = bb_get_forummeta($forum_id, 'allowed_roles');
+    
+    if (!$allowed) {
+        $allowed = array();
+    }
+    
+    ?>
+    <div class="control-group">
+        <label class="control-label">
+            <?php _e('Roles allowed to see this forum:', 'forum_permissions'); ?>
+        </label>
+        <div class="controls">
+    <?php
+    foreach ($bb_roles->roles as $key => $values) { ?>
+            <label class="checkbox"><input type="checkbox" name="allowed_roles[]" value="<?php echo esc_attr($key); ?>" <?php if (isset($allowed[$key])) echo ' checked'; ?> /> <?php _e($values['name']); ?></label>
+    <?php } ?>
+            <p class="help-block"><?php _e('If you don\'t select any role, then everyone will be able to see the forum, even non logged-users.', 'forum_permissions'); ?></p>
+            <p class="help-block"><?php _e('Please note that this filter is not applied to child forums of this forum.', 'forum_permissions'); ?></p>
+        </div>
+    </div>
+    <?php
+}
+
+add_action('bb_forum_form', 'bb_forum_permissions');
+
+
+function bb_forum_permissions_process_form($args) {
+    global $bb_roles;
+    var_dump($args);
+    if (!isset($args['forum_id']))
+        return;
+        
+    $forum_id = intval($args['forum_id']);
+    
+    if (!$forum_id)
+        return;
+        
+    $allowed = array();
+    
+    if (!isset($args['allowed_roles']) or empty($args['allowed_roles']) or !is_array($args['allowed_roles'])) {
+        bb_delete_forummeta($forum_id, 'allowed_roles');
+        return;
+    }
+    
+    foreach ($args['allowed_roles'] as $role) {
+        if (isset($bb_roles->roles[$role]))
+            $allowed[$role] = true;
+    }
+    
+    if ($allowed)
+        bb_update_forummeta($forum_id, 'allowed_roles', $allowed);
+    else 
+        bb_delete_forummeta($forum_id, 'allowed_roles');
+}
+
+add_action('bb_update_forum', 'bb_forum_permissions_process_form');
+
+$bb_forum_permissions_allowed_ids = array();
+
+function bb_forum_permissions_configure_user() {
+    global $bb_current_user;
+    global $bb_forum_permissions_allowed_ids;
+    $logged = bb_is_user_logged_in();
+    
+    $forums = get_forums();
+    foreach ($forums as $forum) {        
+        if (!isset($forum->allowed_roles) || !$forum->allowed_roles) {
+            $bb_forum_permissions_allowed_ids[] = $forum->forum_id;
+        } else if ($logged) {
+            foreach ($bb_current_user->roles as $role) {
+                if (isset($forum->allowed_roles[$role])) {
+                    $bb_forum_permissions_allowed_ids[] = $forum->forum_id;
+                    break;    
+                }
+            }      
+        }
+    }    
+}
+
+add_action('bb_init', 'bb_forum_permissions_configure_user');
+
+
+function bb_forum_permissions_topics_where($sql) {
+    if (is_bb_admin())
+        return;
+        
+    global $bb_forum_permissions_allowed_ids;
+        
+    $sql .= sprintf(" AND t.forum_id IN (%s)", implode(',', $bb_forum_permissions_allowed_ids));
+
+    return $sql;
+}
+
+add_filter('get_topics_where', 'bb_forum_permissions_topics_where');
+
+
+
+function bb_forum_permissions_protect_topic($topic_id) {
+    global $topic;
+    global $bb_forum_permissions_allowed_ids;
+
+    $forum_id = $topic->forum_id;    
+    $allowed_user = in_array($forum_id, $bb_forum_permissions_allowed_ids);
+    
+    if (!$allowed_user) {
+        wp_redirect(bb_get_uri());
+        exit;
+    }
+}
+
+add_action('bb_topic.php_pre_db', 'bb_forum_permissions_protect_topic');
+
