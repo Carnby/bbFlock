0c210459460352a6a05c219561bdfbede792b826
Eduardo Graells <eduardo.graells@gmail.com>
2012-04-29 21:59:45 +0200

    forum permissions plugin: now it filters which forums are displayed on the loop. also it redirects users if they enter into a prohibited forum. finally, now keymasters and admins see everything

--- bb-plugins/bb-forum-permissions/bb-forum-permissions.php
+++ bb-plugins/bb-forum-permissions/bb-forum-permissions.php
@@ -28,7 +28,12 @@ function bb_forum_permissions($forum_id = 0) {
         </label>
         <div class="controls">
     <?php
-    foreach ($bb_roles->roles as $key => $values) { ?>
+    foreach ($bb_roles->roles as $key => $values) { 
+            // keymasters and admins always see everything
+            if ($key == 'keymaster' or $key == 'administrator')
+                continue;
+                
+            ?>
             <label class="checkbox"><input type="checkbox" name="allowed_roles[]" value="<?php echo esc_attr($key); ?>" <?php if (isset($allowed[$key])) echo ' checked'; ?> /> <?php _e($values['name']); ?></label>
     <?php } ?>
             <p class="help-block"><?php _e('If you don\'t select any role, then everyone will be able to see the forum, even non logged-users.', 'forum_permissions'); ?></p>
@@ -99,7 +104,7 @@ add_action('bb_init', 'bb_forum_permissions_configure_user');
 
 function bb_forum_permissions_topics_where($sql) {
     if (is_bb_admin())
-        return;
+        return $sql;
         
     global $bb_forum_permissions_allowed_ids;
         
@@ -126,4 +131,30 @@ function bb_forum_permissions_protect_topic($topic_id) {
 }
 
 add_action('bb_topic.php_pre_db', 'bb_forum_permissions_protect_topic');
+
+
+
+function bb_forum_permissions_protect_forum($forum_id) {
+    global $bb_forum_permissions_allowed_ids;
+  
+    $allowed_user = in_array($forum_id, $bb_forum_permissions_allowed_ids);
+    
+    if (!$allowed_user) {
+        wp_redirect(bb_get_uri());
+        exit;
+    }
+}
+add_action('bb_forum.php_pre_db', 'bb_forum_permissions_protect_forum');
+
+
+function bb_forum_permissions_hide_from_loop($hide) {
+    if (bb_current_user_can('use_keys') || bb_current_user_can('administrate'))
+        return $hide;
+        
+    global $bb_forum_permissions_allowed_ids;
+    $forum_id = get_forum_id();
+    return !in_array($forum_id, $bb_forum_permissions_allowed_ids);
+    
+}
+add_filter('merlot_skip_forum_in_forum_loop', 'bb_forum_permissions_hide_from_loop');
 
