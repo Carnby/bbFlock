9ba4b3df72f1d3c83fe816de221a9803aaa89c73
Eduardo Graells <eduardo.graells@gmail.com>
2012-04-29 22:47:18 +0200

    latest forum topics plugin now caches topics and users correctly

--- bb-plugins/bb-forum-latest-topics/bb-forum-latest-topics.php
+++ bb-plugins/bb-forum-latest-topics/bb-forum-latest-topics.php
@@ -1,13 +1,88 @@
 <?php
 /*
 Plugin Name: Latest Forum Topics
-Plugin URI: http://alumnos.dcc.uchile.cl/~egraells/
+Plugin URI: http://about.me/egraells/
 Description: Shows latest topics per forum.
 Author: Eduardo Graells
-Author URI: http://alumnos.dcc.uchile.cl/~egraells
+Author URI: http://about.me/egraells
 Version: 1.0
 License: GPL3
 */
+
+function _bb_latest_forum_cache_topics($topic_ids) {
+    global $bb_cache, $bb_topic_cache;
+    
+    if (!$topic_ids)
+        return;
+        
+    $bb_cache->cache_topics($topic_ids, false);
+    
+    $poster_ids = array();
+    foreach ($topic_ids as $topic_id) {
+        $topic = $bb_topic_cache[$topic_id];
+        if (!$topic)
+            continue;
+        $poster_ids[$topic->topic_last_poster] = true;
+    }
+    
+    bb_cache_users(array_keys($poster_ids));
+}
+
+function bb_latest_forum_topics_cache() {
+    global $bb_cache;
+    
+    $forums = bb_get_forums_hierarchical(0, 0);
+    
+    $forum_ids = array_keys($forums);
+
+    $topic_ids = array();
+    foreach ($forum_ids as $forum_id) {
+        $topic_id = bb_get_forummeta($forum_id, 'bb_latest_forum_topic');
+        if ($topic_id)
+            $topic_ids[] = $topic_id;
+    }
+    
+    _bb_latest_forum_cache_topics($topic_ids);
+}
+
+function _bb_latest_forum_search($forums, $key) {
+    if (isset($forums[$key]))
+        return array_keys($forums[$key]);
+        
+    foreach ($forums as $subforums) {
+        if (!is_array($subforums))
+            continue;
+        $found = _bb_latest_forum_search($subforums, $key);
+        if ($found !== false && is_array($found))
+            return $found;
+
+    }
+    
+    return false;
+}
+
+function bb_latest_forum_topics_cache_subforums($forum_id) {
+    global $bb_cache;    
+    $forums = bb_get_forums_hierarchical(0, 0);
+    
+    // we have to search for the forum in the array. currently there's no way to perform queries on forums :s
+    $forum_ids = _bb_latest_forum_search($forums, $forum_id);
+    
+    if (!$forum_ids)
+        return;
+        
+    $topic_ids = array();
+    foreach ($forum_ids as $forum_id) {
+        $topic_id = bb_get_forummeta($forum_id, 'bb_latest_forum_topic');
+        if ($topic_id)
+            $topic_ids[] = $topic_id;
+    }
+    
+    _bb_latest_forum_cache_topics($topic_ids);
+}
+
+add_action('bb_index.php', 'bb_latest_forum_topics_cache');
+add_action('bb_forum.php_pre_db', 'bb_latest_forum_topics_cache_subforums');
 
 function bb_latest_forum_topics_update_forum($forum_id) {
     $q = array(
@@ -18,8 +93,10 @@ function bb_latest_forum_topics_update_forum($forum_id) {
 
     $query = new BB_Query( 'topic', $q, 'get_latest_forum_topic' );
     
-    if (!$query->results)
+    if (!$query->results) {
+        bb_delete_forummeta($forum_id, 'bb_latest_forum_topic');
         return;
+    }
 
     $topic = $query->results[0];
     
@@ -75,6 +152,7 @@ function bb_latest_forum_topics_header() {
 }
 
 function bb_latest_forum_topics_get_last_topic($forum_id) {
+    //return;
 	$topic_id = bb_get_forummeta($forum_id, 'bb_latest_forum_topic');
 	if (!$topic_id)
 	    return;
@@ -92,9 +170,9 @@ function bb_latest_forum_topics_loop_header() {
     printf('<th class="forum_latest_topics_header">%s</th>', __('Latest Post', 'bb-forum-latest-topics'));
 }
 
-function bb_latest_forum_topics_loop() {
+function bb_latest_forum_topics_loop($forum_id) {
     printf('<td class="forum_latest_topics_loop">');
-    bb_latest_forum_topics_get_last_topic(get_forum_id());
+    bb_latest_forum_topics_get_last_topic($forum_id);
     printf('</td>');
 }
 
