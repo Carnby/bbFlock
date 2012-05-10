2d8496d256dd8c6dcfcfcaae699d002c7cbc45c6
Eduardo Graells <eduardo.graells@gmail.com>
2012-05-10 22:11:29 +0200

    post-form now uses the bootstrap classes for form elements

--- bb-templates/merlot/post-form.php
+++ bb-templates/merlot/post-form.php
@@ -1,30 +1,51 @@
 
 
 <?php if ( is_bb_tag() || is_front() ) { ?>
-    <label for="forum-id"><?php _e('Pick a section:'); ?><?php bb_new_topic_forum_dropdown(); ?></label>
+    <div class="control-group">
+        <label for="forum-id"><?php _e('Pick a section:'); ?></label>
+        <div class="controls">
+            <?php bb_new_topic_forum_dropdown(); ?>
+        </div>
+    </div>
 <?php } ?>
 
 <?php if ( !is_topic() ) : ?>
 
+    <div class="control-group">
+	    <label for="topic"><?php _e('Topic title: (be brief and descriptive)'); ?></label>
+		<div class="controls">
+		    <input name="topic" type="text" id="topic" class="input-xlarge" size="50" maxlength="80" tabindex="1" />
+	    </div>
+	</div>
 
-	<label for="topic"><?php _e('Topic title: (be brief and descriptive)'); ?>
-		<input name="topic" type="text" id="topic" size="50" maxlength="80" tabindex="1" />
-	</label>
+    <?php endif; ?>
 
-<?php endif; do_action( 'post_form_pre_post' ); ?>
 
-	<label for="post_content"><?php _e('Post:'); ?>
-		<textarea class="span10" name="post_content" cols="50" rows="12" id="post_content" tabindex="3"></textarea>
-	</label>
-	<?php do_action('post_form_after_post'); ?>
-
+    <div class="control-group">
+	    <label for="post_content"><?php _e('Post:'); ?></label>
+	    <div class="controls">
+	        <?php do_action( 'post_form_pre_post' ); ?>
+		    <textarea class="span10" name="post_content" cols="50" rows="12" id="post_content" tabindex="3" class="input-xlarge"></textarea>
+		    <?php do_action('post_form_after_post'); ?>
+	    </div>
+    </div>
 <?php if ( !is_topic() ) : ?>
-	<label for="tags-input"><?php printf(__('Enter a few words (called <a href="%s">tags</a>) separated by commas to help someone find your topic:'), bb_get_tag_page_link()) ?>
-		<input id="tags-input" name="tags" type="text" size="50" maxlength="100" value="<?php bb_tag_name(); ?> " tabindex="4" />
-	</label>
+
+    <div class="control-group">
+	<label for="tags-input"><?php _e('Tags'); ?></label>
+	    <div class="controls">
+	        <input id="tags-input" name="tags" type="text" size="50" maxlength="100" class="input-xlarge" value="<?php bb_tag_name(); ?> " tabindex="4" />
+	        <p class="help-block"><?php printf(__('Enter a few words (called <a href="%s">tags</a>) separated by commas to help someone find your topic:'), bb_get_tag_page_link()); ?></p>
+	    </div>
+	</div>
 
 <?php endif; ?>
-  <input class="btn btn-primary"type="submit" id="postformsub" name="Submit" value="<?php echo attribute_escape( __('Send Post &raquo;') ); ?>" tabindex="4" />
+    <div class="form-actions">
+        <input class="btn btn-primary"type="submit" id="postformsub" name="Submit" value="<?php echo attribute_escape( __('Send Post &raquo;') ); ?>" tabindex="4" />
+        <?php do_action('post_form_buttons'); ?>
+    </div>
+  
+  
 
-<?php gs_post_form_help(); ?>
+
 
