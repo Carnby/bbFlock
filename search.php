77795a246ec3221f8e0fdb82ae1ed83a428990b8
Eduardo Graells <eduardo.graells@gmail.com>
2012-05-16 21:17:55 +0200

    removing vardump from search controller

--- search.php
+++ search.php
@@ -9,7 +9,6 @@ $bb_query_form = new BB_Query_Form;
 if ( $q = stripslashes( $q ) ) {
 	$bb_query_form->BB_Query_Form( 'topic', array( 'search' => $q ), array( 'post_status' => 0, 'topic_status' => 0, 'search', 'forum_id', 'tag', 'topic_author', 'post_author' ), 'bb_relevant_search' );
 	$relevant = $bb_query_form->results;
-    var_dump($relevant);
 	$q = $bb_query_form->get( 'search' );
 }
 
