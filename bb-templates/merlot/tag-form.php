d0c793a01e949e3a3d47c35dcd2ae6a946a23d3b
Eduardo Graells <eduardo.graells@gmail.com>
2012-08-07 21:13:23 -0700

    tag input validation on topics

--- bb-templates/merlot/tag-form.php
+++ bb-templates/merlot/tag-form.php
@@ -1,3 +1,13 @@
+
 <input name="tag" type="text" id="tag" size="10" maxlength="30" class="input-large" placeholder="<?php _e('Tag'); ?>"/>
 <button type="submit" name="submit" class="btn btn-primary"><i class="icon icon-tags icon-white"></i> <?php _e('Add Tags'); ?></button>
 <input type="hidden" name="id" value="<?php topic_id(); ?>" />
+
+<script type="text/javascript">
+$('#tag-form').on('submit', function() {
+    var current_tag = $('#tag').attr('value');
+    if (!current_tag) {
+        return false;
+    }
+});
+</script>
