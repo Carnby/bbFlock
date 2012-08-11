e9c5ca572a360212d1db3ea7a6ee85fa129f7f3c
Eduardo Graells <eduardo.graells@gmail.com>
2012-08-11 00:58:10 -0700

    new design for the memberlist page

--- bb-templates/merlot/memberlist.php
+++ bb-templates/merlot/memberlist.php
@@ -1,23 +1,48 @@
 <?php bb_get_header(); ?>
 
-<?php if ($members) { ?>
-
 <?php merlot_user_views_tabs(); ?>
 
-<div class="members">
+<?php if ($members) { 
+    $current_member = 0;
+?>
 
+<div class="members">
+<hr/>
 <?php foreach ($members as $member) { ?>
-    <div class="member">
-	    <div class="pull-left" style="width: 80px;">
-	        <?php echo bb_get_avatar($member->ID, 64); ?>
-	    </div>
-	    <div class="profile-info">
-	        <h5><?php echo "<a href=\"".get_user_profile_link($member->ID)."\">".$member->user_login."</a>"; ?></h5>
-	        <p><span class="label label-info"><?php echo get_user_type($member->ID); ?></span></p>
-	    </div>
-	    
-	    <hr />
-	 </div>
+
+    <?php if ($current_member == 0) { 
+    ?>
+    <div class="row">
+    <?php } 
+     ++$current_member;
+    ?>
+        <div class="span4">
+            <div class="row">
+	            <div class="span1">
+	                <?php echo bb_get_avatar($member->ID, 64); ?>
+	            </div>
+	            <div class="span3"">
+	                <h5><?php echo "<a href=\"".get_user_profile_link($member->ID)."\">".$member->user_login."</a>"; ?> <span class="label label-info"><?php echo get_user_type($member->ID); ?></span></h5>
+	                <?php bb_profile_data($member->ID); ?>
+	            </div>
+	           </div>
+	     </div>
+	 
+    <?php if ($current_member == 3) { ?>
+        	<div class="clearfix"></div>
+	        <hr />
+    </div>
+    <?php 
+        $current_member = 0;
+    } 
+    
+    ?>
+<?php } ?>
+
+<?php if ($current_member != 0) { ?>
+    	<div class="clearfix"></div>
+        <hr />
+</div>
 <?php } ?>
 
 </div>
